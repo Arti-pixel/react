e0ae2a47d5405af5740908b20396a410929457c0
Arti-pixel <Artur354546@yandex.ru>
2023-04-02 01:36:19 +0300

    mobile styling + first page

--- src/App.js
+++ src/App.js
@@ -4,108 +4,21 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee.js";
 import EditEmployee from "./components/EditEmployee.js";
+import Header from "./components/Header.js";
+import Employees from "./pages/Employees.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Customers from "./pages/Customers";
 
 function App() {
-  const [role, setRole] = useState("dev");
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      name: "Caleb",
-      role: "YouTube Sensation",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
-    },
-    {
-      id: 2,
-      name: "Sal",
-      role: "Manager",
-      img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg",
-    },
-    {
-      id: 3,
-      name: "John",
-      role: "Director of Eng.",
-      img: "https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg",
-    },
-    {
-      id: 4,
-      name: "Melanie",
-      role: "Software Engineer",
-      img: "https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg",
-    },
-    {
-      id: 5,
-      name: "Corey",
-      role: "The Devops Guy",
-      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
-    },
-    {
-      id: 6,
-      name: "Jake",
-      role: "Senior",
-      img: "https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg",
-    },
-  ]);
-
-  function updateEmployee(id, newName, newRole) {
-    const updateEmployees = employees.map((employee) => {
-      if (id == employee.id) {
-        return { ...employee, name: newName, role: newRole };
-      }
-
-      return employee;
-    });
-    setEmployees(updateEmployees);
-  }
-
-  function newEmployee(name, role, img) {
-    const newEmployee = {
-      id: uuidv4(),
-      name: name,
-      role: role,
-      img: img,
-    };
-    setEmployees([...employees, newEmployee]);
-  }
-
-  const showEmployees = true;
   return (
-    <div className="App">
-      {showEmployees ? (
-        <>
-          <input
-            type="text"
-            onChange={(e) => {
-              setRole(e.target.value);
-            }}
-          ></input>
-          <div className="flex flex-wrap justify-center">
-            {employees.map((employee) => {
-              const editEmployee = (
-                <EditEmployee
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  updateEmployee={updateEmployee}
-                />
-              );
-              return (
-                <Employee
-                  key={employee.id}
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  img={employee.img}
-                  editEmployee={editEmployee}
-                />
-              );
-            })}
-          </div>
-          <AddEmployee NewEmployee={newEmployee} />
-        </>
-      ) : (
-        <p>You cannot see the employees</p>
-      )}
-    </div>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/customers" element={<Customers />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
