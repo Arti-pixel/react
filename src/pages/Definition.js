ede306326cd33f0c7b15b4c84810c545a472244e
Arti-pixel <Artur354546@yandex.ru>
2023-04-02 20:42:31 +0300

    style the dictionary

---
+++ src/pages/Definition.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import DefinitionSearch from "../components/DefinitionSearch";
+import NotFound from "../components/NotFound";
+
+export default function Definition() {
+  const [word, setWord] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
+
+  let { search } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // const url = "https://frmergmrkgemfewefewfhncvcbvcreg.com";
+    // const url = "https://httpstat.us/500";
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
+    fetch(url)
+      .then((response) => {
+        if (response.status === 404) {
+          setNotFound(true);
+        } else if (response.status === 401) {
+          navigate("/login");
+        } else if (response.status === 500) {
+          //   setServerError(true);
+        }
+        if (!response.ok) {
+          setError(true);
+
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setWord(data[0].meanings);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
+
+  if (notFound === true) {
+    return (
+      <>
+        <NotFound />
+        <Link to="/dictionary">Search another</Link>
+      </>
+    );
+  }
+
+  if (error === true) {
+    return (
+      <>
+        <p>Something went wrong, try again?</p>
+        <Link to="/dictionary">Search another</Link>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {word ? (
+        <>
+          <h1>Here is a definition</h1>
+          {word.map((meaning) => {
+            return (
+              <p key={uuidv4()}>
+                {meaning.partOfSpeech + " "}:{meaning.definitions[0].definition}
+              </p>
+            );
+          })}
+          <p>Search again:</p>
+          <DefinitionSearch />
+        </>
+      ) : null}
+    </>
+  );
+}
